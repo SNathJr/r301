47ce1522c1d1126c0d3f89c3194e478c27dd0f05
Swastik Nath <swastik.nath@live.com>
2020-03-02 03:40:45 +0530

    Static files fix

--- r301/settings.py
+++ r301/settings.py
@@ -130,8 +130,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Django - Bootstrap message tags
 MESSAGE_TAGS = {
